2048583604cc293e418e6635c263bf571928ab3b
Julian Egbert <jjegbe01@louisville.edu>
2015-09-12 19:12:06 -0400

    Added image display button + method

--- app/src/main/java/jjegbe01/myapplication/DisplayAndroidActivity.java
+++ app/src/main/java/jjegbe01/myapplication/DisplayAndroidActivity.java
@@ -1,29 +1,31 @@
 package jjegbe01.myapplication;
 
 import android.content.Intent;
-import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
+import android.widget.ImageView;
 
-public class MyActivity extends AppCompatActivity {
+import static jjegbe01.myapplication.R.drawable.androidicon;
 
-    public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
-
+public class DisplayAndroidActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
+
+        Intent intent = getIntent();
+
+        ImageView imageView = new ImageView(this);
+        imageView.setImageResource(androidicon);
+        setContentView(imageView);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_my, menu);
+        getMenuInflater().inflate(R.menu.menu_display_android, menu);
         return true;
     }
 
@@ -41,20 +43,4 @@ public class MyActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-    /** Called when the user clicks the Send button */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }
-
-    public void displayImage(View view)
-    {
-        Intent intent = new Intent(this, DisplayAndroidActivity.class);
-        startActivity(intent);
-    }
-
 }
